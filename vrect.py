1e9856cd8df6cb8f1a7445fe84bf9f92a087a50a
승연 <91962051+Piop2@users.noreply.github.com>
2023-09-14 02:09:55 +0900

    Update vrect.py

--- vrect.py
+++ vrect.py
@@ -6,4 +6,11 @@ author: Piop2
 
 
 class VectorRect:
-    pass
+    def __init__(self, x: float, y: float, width: float, height:float, angle: float = 0) -> None:
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.angle = angle
+
+
