d34d95f1412b2af63d1fafd694f4d3729795e464
piop2 <pioooop2@gmail.com>
2023-09-18 00:42:43 +0900

    add example for test

--- example/rect_spin.py
+++ example/rect_spin.py
@@ -7,22 +7,22 @@ surf = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Vector Rect test")
 clock = pygame.time.Clock()
 
-vrect = VRect(250, 250, 100, 100, 45)
+vrect = VRect(250, 250, 100, 100)
 
-dot = [250, 0]
-
+dot = (250, 190)
 
 running = True
 while running:
     clock.tick(60)
 
-    surf.fill("white")
-    vrect.draw(surf, "black")
+    surf.fill("black")
+    vrect.draw(surf, "white", 0, True) if not vrect.collidepoint(*dot) else vrect.draw(
+        surf, "green", 0, True
+    )
+
     pygame.draw.circle(surf, "red", dot, 5)
 
-    dot[1] += 1
-
-    print(vrect.collidepoint(*dot), dot)
+    vrect.a += 0.5
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
